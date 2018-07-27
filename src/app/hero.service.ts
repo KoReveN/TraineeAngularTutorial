198e8d7e12a86cf4ceeb7df094ecb1522a573ea9
koreven <koreven@gmail.com>
2018-07-27 09:50:31 +0300

    add error hendler

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -5,6 +5,7 @@ import { Observable, of } from 'rxjs';
 import { MessageService } from './message.service';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
+import { catchError, map, tap } from 'rxjs/operators';
 
 
 @Injectable({
@@ -21,10 +22,11 @@ export class HeroService {
 
   // constructor(private messageService: MessageService) { }
 
+
   getHero(id: number): Observable<Hero> {
-      // TODO: send the message _after_ fetching the hero
-  this.messageService.add(`HeroService: fetched hero id=${id}`);
-  return of(HEROES.find(hero => hero.id === id));
+    // TODO: send the message _after_ fetching the hero
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(HEROES.find(hero => hero.id === id));
   }
 
   // getHeroes(): Observable<Hero[]> {
@@ -34,13 +36,44 @@ export class HeroService {
   // }
 
   /** GET heroes from the server */
-getHeroes (): Observable<Hero[]> {
-  return this.http.get<Hero[]>(this.heroesUrl);
-}
+  // getHeroes (): Observable<Hero[]> {
+  //   return this.http.get<Hero[]>(this.heroesUrl);
+  // }
+
+  getHeroes(): Observable<Hero[]> {
+    return this.http.get<Hero[]>(this.heroesUrl)
+      .pipe(
+        catchError(this.handleError('getHeroes', []))
+      );
+  }
 
   /** Log a HeroService message with the MessageService */
-private log(message: string) {
-  this.messageService.add(`HeroService: ${message}`);
-}
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
+  }
+
+
+
+  /**
+   * Handle Http operation that failed.
+   * Let the app continue.
+   * @param operation - name of the operation that failed
+   * @param result - optional value to return as the observable result
+   */
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+      this.log(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
+
+
 
 }
